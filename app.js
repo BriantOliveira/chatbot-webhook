b75abde7b458c8751b4082bcf1bd6cd7ea551781
Elliot Briant <briantmoliveira@gmail.com>
2018-05-24 09:19:57 -0700

    main server structure

---
+++ app.js
@@ -0,0 +1,29 @@
+/*********************************
+*     WEBHOOK CHATBOT API
+*********************************/
+
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const http = require('http');
+const expressSanitizer = require('express-sanitizer');
+const API_KEY = process.env.SECRETKEY;
+
+//Initializing server
+const app = express();
+
+//Defining the PORT connection
+const PORT = process.env.PORT || 3000
+
+//Middleware
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(expressSanitizer());
+
+//ROUTE LOAD
+
+
+//Port Listener
+app.listen(PORT, function() {
+  console.log('WEBHOOK API LISTENING ON PORT', PORT);
+});
